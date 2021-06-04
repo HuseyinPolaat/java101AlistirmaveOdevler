8a8944b566069c3f957aba30b02e2224116ac4f5
HuseyinPolaat <usainpolat@gmail.com>
2021-06-04 11:18:16 +0300

    $ git commit -m

---
+++ fibonacciRecursive/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    static int fib(int n){
+        if (n == 1 || n == 0){
+            return 1;
+        }
+        return fib(n -1 ) + fib(n - 2);
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Kaç fibonacci sayısı görmek istersin?: ");
+        int numb = input.nextInt();
+
+        for (int i = 0; i < numb; i++){
+            System.out.println(fib(i));
+        }
+    }
+}
